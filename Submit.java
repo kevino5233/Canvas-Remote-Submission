d9c457141ebba6f9430672f9973288055ad3cd40
Kevin Sun <kevinosu@cs.utexas.edu>
2014-10-15 11:26:18 -0500

    Some directory changes, going to start using eclipse instead.

--- Submit.java
+++ Submit.java
@@ -1,3 +1,4 @@
+import org.json;
 import java.net.*;
 import java.io.*;
 
@@ -6,19 +7,19 @@ public class Submit{
 	//arbitrary?
 	BufferedReader oauthKeyFile = new BufferedReader(new FileReader("oauth.txt"));
 	String oauthKey = oauthKeyFile.readLine();
-	System.out.println(oauthKey);
 	
 	//changed to use oauth key. remove this stuff and replace with the post methods described on Canvas's API
 	
-	URL authRequest = new URL("https://utexas.instructure.com/login/oauth2/auth?");
-	System.out.println(authRequest);
+	URL authRequest = new URL("https://canvas.instructure.com/api/v1/courses?access_token=" + oauthKey);
 	URLConnection authRequestConnection = authRequest.openConnection();
 	InputStream authRequestResponse = authRequestConnection.getInputStream();
 	int inputByte = authRequestResponse.read();
+	String courseRawJSON = "";	
 	while (inputByte != -1){
-	   System.out.println((char)inputByte);
+	   courseRawJSON += (char)inputByte;
 	   inputByte = authRequestResponse.read();
 	}
-
+	JSONObject courseJSON = new JSONObject(courseRawJSON);
+	System.out.println(courseJSON);	
     }
 }
